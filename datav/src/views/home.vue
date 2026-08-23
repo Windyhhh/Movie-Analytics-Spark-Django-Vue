cc507f55069fde9d608bead22061293f4855f48d
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 17:06:30 +0800

    Add datav/src/views/home.vue

---
+++ datav/src/views/home.vue
@@ -0,0 +1,17 @@
+<script setup>
+ import ModuleFirst from './moduleFirst.vue';
+import ModuleSecond from './moduleSecond.vue';
+ import ModuleTitle from './moduleTitle.vue';
+</script>
+
+<template>
+  <dv-full-screen-container>
+      <ModuleTitle/>
+      <ModuleFirst/>
+      <ModuleSecond/>
+  </dv-full-screen-container>
+</template>
+
+<style scoped>
+
+</style>

80e45c3d6b13a6176ccd518cc8e3409d242c65d5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 17:08:45 +0800

    Add movie_analytics/frontend/src/views/chart/water.vue

---
+++ movie_analytics/frontend/src/views/chart/water.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useDataStore } from '../../stores/data';
+const dataStore = useDataStore()
+const config = ref({
+  data:[55,45],
+  shape:'roundRect'
+})
+
+</script>
+
+<template>
+  <dv-water-level-pond :config="config" style="width: 100%;height: 150px; "></dv-water-level-pond>
+</template>
+
+<style scoped>
+
+</style>

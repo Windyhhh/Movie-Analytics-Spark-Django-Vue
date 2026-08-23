09345c6376115820786b8e87ec181adf65269fec
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 17:08:33 +0800

    Add movie_analytics/frontend/src/views/moduleSecond.vue

---
+++ movie_analytics/frontend/src/views/moduleSecond.vue
@@ -0,0 +1,27 @@
+<script setup>
+ import { ref } from 'vue';
+ import Barchart from './chart/barchart.vue';
+ import Linechart from './chart/linechart.vue';
+ import scrollboard from './chart/scrollboard.vue';
+</script>
+
+<template>
+  <div class="d-flex">
+    <div style="flex:0 1 50%">
+    <dv-border-box-13 style="width: 100%;height: 350px;">
+      <Barchart></Barchart>
+      <!-- <scrollboard></scrollboard> -->
+    </dv-border-box-13>
+  </div>
+   
+  <div style="flex:0 1 50%">
+    <dv-border-box-12 style="width:100%;height: 350px;">
+      <Linechart></Linechart>
+    </dv-border-box-12>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

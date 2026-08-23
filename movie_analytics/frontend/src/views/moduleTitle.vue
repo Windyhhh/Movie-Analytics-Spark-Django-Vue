156832fdcdafd2e2629934fc181d5e74a3895ec5
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 17:08:34 +0800

    Add movie_analytics/frontend/src/views/moduleTitle.vue

---
+++ movie_analytics/frontend/src/views/moduleTitle.vue
@@ -0,0 +1,31 @@
+<script setup>
+ 
+</script>
+
+<template>
+  <div class="module-box">
+    <div style="flex:0 1 30%">
+      <div small-bg>
+          <dv-decoration-10 style="width: 100%;height: 5px;" />
+      </div>
+    </div>
+    <div style="flex: 0 1 40%;"> 
+      <div style="display: flex;">
+    
+        <dv-decoration-8 style="width:300px;height: 50px; flex: 1;" />
+        <div class="index-title" style="color: aqua;">北上广深房屋租赁分析可视化</div>
+        <dv-decoration-8 :reverse="true" style="width:300px;height: 50px; flex: 1;" />
+      
+      </div>
+    </div>
+    <div style="flex:0 1 30%">
+      <div small-bg>
+          <dv-decoration-10 style="width: 100%;height: 5px;" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
